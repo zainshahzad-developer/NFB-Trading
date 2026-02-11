4ae068732f8876ed7696cb327407820624782bf4
Hina Shahzad <NFBTradingLimited@Hinas-MacBook-Air.local>
2026-02-11 13:19:38 +0500

    Fix: Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/NFB-Trading">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/stock" element={<Stock />} />
